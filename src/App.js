7abe8d48daa3cbe41c5c6c029b443a9412554259
song <gganggilove@naver.com>
2019-11-13 14:38:02 +0900

    data delete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, {Component} from 'react';
 import PhoneForm from './component/PhoneForm';
+import PhoneInfoList from './component/PhoneInfoList';
 
 class App extends Component{
-  handleCreate = (data) => {
-    console.log(data);
+  id = 2
+  state = {
+    information:[
+      {
+        id:0,
+        name:'김민준',
+        phone:'010-0000-0000'
+      },
+      {
+        id:1,
+        name:'홍길동',
+        phone: '010-0000-0001'
+      }
+    ]
   }
+  handleCreate = (data) => {
+    /*console.log(data);*/
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({id:this.id++, ...data})
+    })
+  }
+  handleRemove = (id) => {
+    const {information} = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }  
   render(){
+    const { information } = this.state;
     return (
       <div>
         <PhoneForm
           onCreate={this.handleCreate}
         />
+        {/*{JSON.stringify(information)}*/}
+        <PhoneInfoList 
+          /*data={this.state.information}*/
+          data={information}
+          onRemove = {this.handleRemove}
+        />
       </div>
-    )
+    );
   }
 }
 
