72f5983e1afa455e0810d4c4c7a555491d1c0aaa
song <gganggilove@naver.com>
2019-11-20 10:41:38 +0900

    Immutability

--- src/App.js
+++ src/App.js
@@ -16,7 +16,13 @@ class App extends Component{
         name:'홍길동',
         phone: '010-0000-0001'
       }
-    ]
+    ],
+    keyword:''
+  }
+  handleChange = (e) => {
+    this.setState({
+      keyword:e.target.value,
+    });
   }
   handleCreate = (data) => {
     /*console.log(data);*/
@@ -42,16 +48,28 @@ class App extends Component{
     })
   }
   render(){
-    const { information } = this.state;
+    const { information, keyword } = this.state;
+    const filteredList = information.filter(
+      info => info.name.indexOf(keyword) !== -1
+    );
     return (
       <div>
         <PhoneForm
           onCreate={this.handleCreate}
         />
         {/*{JSON.stringify(information)}*/}
+        <p>
+          <input
+            placeholder="검색 할 이름을 입력하세요."
+            onChange={this.handleChange}
+            value={keyword}
+          />
+        </p>
+        <hr/>
         <PhoneInfoList 
           /*data={this.state.information}*/
-          data={information}
+          /*data={information}*/
+          data ={filteredList}
           onRemove = {this.handleRemove}
           onUpdate = {this.handleUpdate}
         />
